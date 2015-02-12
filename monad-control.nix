cb624c38fc03e7930040370347766f6fad6b7d54
Bas van Dijk <v.dijk.bas@gmail.com>
2015-02-12 20:43:55 +0100

    Bump version to 1.0.0.3

--- monad-control.nix
+++ monad-control.nix
@@ -1,10 +1,10 @@
-{ cabal, transformers, transformersBase }:
+{ cabal, transformers, transformersBase, transformersCompat }:
 
 cabal.mkDerivation (self: {
   pname = "monad-control";
   version = "HEAD";
   src = ./.;
-  buildDepends = [ transformers transformersBase ];
+  buildDepends = [ transformers transformersBase transformersCompat ];
   meta = {
     homepage = "https://github.com/basvandijk/monad-control";
     description = "Lift control operations, like exception catching, through monad transformers";
